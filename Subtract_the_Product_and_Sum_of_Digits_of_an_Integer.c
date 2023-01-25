070a3d79fb7427c4c23caa7b8a387a277cb2297c
padmarajugundra <119720413+padmarajugundra@users.noreply.github.com>
2023-01-25 18:51:48 +0530

    // sum and product of gn #include<stdio.h> int main() {     int n,s=0,p=1,i,diff,q,r;     scanf("%d",&n);       q=n;       while(q!=0)       {           r=q%10;           p=p*r;           s+=r;           q=q/10;       }       diff=(p-s);       printf("%d",diff); }

--- Subtract_the_Product_and_Sum_of_Digits_of_an_Integer.c
+++ Subtract_the_Product_and_Sum_of_Digits_of_an_Integer.c
@@ -1,16 +1,17 @@
+// sum and product of gn
 #include<stdio.h>
 int main()
 {
-    int i,diff,n,q,r,s=0,p=1;
+    int n,s=0,p=1,i,diff,q,r;
     scanf("%d",&n);
-    q=n;
-    while(q!=0)
-    {
-        r=q%10;
-        p=p*r;
-        s+=r;
-        q=q/10;
-    }
-    diff=(p-s);
-    printf("%d",diff);
-}        
+      q=n;
+      while(q!=0)
+      {
+          r=q%10;
+          p=p*r;
+          s+=r;
+          q=q/10;
+      }
+      diff=(p-s);
+      printf("%d",diff);
+}
